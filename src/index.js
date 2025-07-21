f365abb3a0de62a7b58ff89e0b4144844b8af11e
Saron <saronabebe2019@gmail.com>
2025-07-21 13:22:12 -0800

    Add more fields to the mock-backend information on the songs

--- src/index.js
+++ src/index.js
@@ -11,33 +11,140 @@ createServer({
   },
 
   seeds(server) {
-    server.create("song", { title: "Easy On Me", artist: "Adele" });
-    server.create("song", { title: "Peaches", artist: "Justin Bieber" });
-    server.create("song", { title: "Blinding Lights", artist: "The Weeknd" });
-    server.create("song", { title: "Levitating", artist: "Dua Lipa" });
+    server.create("song", {
+      title: "Easy On Me",
+      artist: "Adele",
+      album: "30",
+      year: 2021,
+      genre: "Pop",
+      duration: "3:44",
+    });
+
+    server.create("song", {
+      title: "Peaches",
+      artist: "Justin Bieber",
+      album: "Justice",
+      year: 2021,
+      genre: "R&B",
+      duration: "3:18",
+    });
+
+    server.create("song", {
+      title: "Blinding Lights",
+      artist: "The Weeknd",
+      album: "After Hours",
+      year: 2020,
+      genre: "Synth-pop",
+      duration: "3:20",
+    });
+
+    server.create("song", {
+      title: "Levitating",
+      artist: "Dua Lipa",
+      album: "Future Nostalgia",
+      year: 2020,
+      genre: "Disco-pop",
+      duration: "3:23",
+    });
+
     server.create("song", {
       title: "Watermelon Sugar",
       artist: "Harry Styles",
+      album: "Fine Line",
+      year: 2019,
+      genre: "Pop rock",
+      duration: "2:53",
     });
-    server.create("song", { title: "Save Your Tears", artist: "The Weeknd" });
+
+    server.create("song", {
+      title: "Save Your Tears",
+      artist: "The Weeknd",
+      album: "After Hours",
+      year: 2020,
+      genre: "Synth-pop",
+      duration: "3:35",
+    });
+
     server.create("song", {
       title: "drivers license",
       artist: "Olivia Rodrigo",
+      album: "SOUR",
+      year: 2021,
+      genre: "Pop",
+      duration: "4:02",
     });
+
     server.create("song", {
       title: "Still Here",
       artist: "Seungmin (StrayKids)",
+      album: "SKZ-RECORD",
+      year: 2023,
+      genre: "K-Pop",
+      duration: "3:30",
     });
-    server.create("song", { title: "good 4 u", artist: "Olivia Rodrigo" });
-    server.create("song", { title: "Bad Habits", artist: "Ed Sheeran" });
+
+    server.create("song", {
+      title: "good 4 u",
+      artist: "Olivia Rodrigo",
+      album: "SOUR",
+      year: 2021,
+      genre: "Pop rock",
+      duration: "2:58",
+    });
+
+    server.create("song", {
+      title: "Bad Habits",
+      artist: "Ed Sheeran",
+      album: "=",
+      year: 2021,
+      genre: "Dance-pop",
+      duration: "3:50",
+    });
+
     server.create("song", {
       title: "Face",
       artist: "GOT7",
+      album: "Call My Name",
+      year: 2019,
+      genre: "K-Pop",
+      duration: "3:12",
     });
-    server.create("song", { title: "Butter", artist: "BTS" });
-    server.create("song", { title: "Shivers", artist: "Ed Sheeran" });
-    server.create("song", { title: "As It Was", artist: "Harry Styles" });
-    server.create("song", { title: "Anti-Hero", artist: "Taylor Swift" });
+
+    server.create("song", {
+      title: "Butter",
+      artist: "BTS",
+      album: "Butter",
+      year: 2021,
+      genre: "Dance-pop",
+      duration: "2:44",
+    });
+
+    server.create("song", {
+      title: "Shivers",
+      artist: "Ed Sheeran",
+      album: "=",
+      year: 2021,
+      genre: "Pop rock",
+      duration: "3:27",
+    });
+
+    server.create("song", {
+      title: "As It Was",
+      artist: "Harry Styles",
+      album: "Harry's House",
+      year: 2022,
+      genre: "Synth-pop",
+      duration: "2:47",
+    });
+
+    server.create("song", {
+      title: "Anti-Hero",
+      artist: "Taylor Swift",
+      album: "Midnights",
+      year: 2022,
+      genre: "Synth-pop",
+      duration: "3:20",
+    });
   },
 
   routes() {
@@ -45,6 +152,11 @@ createServer({
 
     this.get("/songs", (schema) => {
       return schema.songs.all();
+    });
+
+    this.get("/songs/:id", (schema, request) => {
+      let id = request.params.id;
+      return schema.songs.find(id);
     });
 
     this.post("/songs", (schema, request) => {
